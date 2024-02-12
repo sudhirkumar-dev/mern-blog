d9222875296591337d7cc0b84f80ee0d480c3860
sudhirkumar-dev <kalivarapuskumar@gmail.com>
2024-02-12 16:48:53 +0530

    comment started

---
+++ client/src/components/Comment.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react'
+
+export default function Comment({comment}) {
+    useEffect(()=>{
+        
+    },[comment])
+  return (
+    <div>Comment</div>
+  )
+}
