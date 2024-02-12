5e4fa5b2ed18c623af180ccf434639aaabe1c6e4
sudhirkumar-dev <kalivarapuskumar@gmail.com>
2024-02-12 16:23:14 +0530

    debugged comment section

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,4 +1,4 @@
-import { Button, Textarea } from "flowbite-react";
+import { Alert, Button, Textarea } from "flowbite-react";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -6,24 +6,32 @@ import { Link } from "react-router-dom";
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
-  const handleSubmit = async (e) => {
+  const [commentError, setCommentError] = useState(null);
+  const handleSubmitComment = async (e) => {
+    e.preventDefault();
     if (comment.length > 200) {
       return;
     }
-    const res = await fetch("/api/comment/create", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        content: comment,
-        postId,
-        userId: currentUser._id,
-      }),
-    });
-    const  data = await res.json();
-    if(res.ok){
-      setComment('')
+    try {
+      const res = await fetch("/api/comment/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: comment,
+          postId,
+          userId: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setComment("");
+        setCommentError(null);
+      }
+      console.log(data);
+    } catch (error) {
+      setCommentError(error.message);
     }
   };
 
@@ -55,7 +63,7 @@ export default function CommentSection({ postId }) {
       {currentUser && (
         <form
           className="border border-teal-500 rounded-md p-3"
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmitComment}
         >
           <Textarea
             placeholder="Add a comment..."
@@ -70,10 +78,15 @@ export default function CommentSection({ postId }) {
             <p className="text-gray-500 text-xs">
               {200 - comment.length} characters remaining
             </p>
-            <Button outline gradientDuoTone="purpleToBlue">
+            <Button outline gradientDuoTone="purpleToBlue" type="submit">
               Submit
             </Button>
           </div>
+          {commentError && (
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
+          )}
         </form>
       )}
     </div>
