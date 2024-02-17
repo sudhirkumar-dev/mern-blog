e8f72144c0bf02bc27407a0e4c867b8af4fc3a0c
sudhirkumar-dev <kalivarapuskumar@gmail.com>
2024-02-17 16:25:22 +0530

    updated projects and about

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -5,6 +5,9 @@ export default function Projects() {
     <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
       <h1 className='text-3xl font-semibold'>Projects</h1>
       <p className='text-md text-gray-500'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+      <p>Some of my own projects  that I've worked on.</p>
+      <iframe src="https://quick-estate2.onrender.com" frameborder="0"></iframe>
+      <p>A Listing Website for Quickly finding a home of our dreams and adding our own listing</p>
       <CallToAction />
     </div>
   )
